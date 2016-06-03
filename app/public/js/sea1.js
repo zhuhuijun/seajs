945a50b0cfd84b823c825f69d3fdd7aab63e1482
zhuhuijun <zhuhjtcx@qq.com>
2016-06-03 23:36:22 +0800

    module

--- app/public/js/sea1.js
+++ app/public/js/sea1.js
@@ -1,6 +1,21 @@
 /**
  * Created by huijun on 2016/6/3.
  */
-define(function(require,exports,module){
-    
-})
+define(function (require, exports, module) {
+
+    var dialog= function () {
+        console.info("func dialog...");
+    }
+
+    var b = require('./b');
+    b.bdialog();
+    //导出模块
+    exports.dialog = dialog;
+});
+//独立模块,直接导出
+/*
+ define({
+ dialog:function(){
+ console.info("func dialog...");
+ }
+ })*/
