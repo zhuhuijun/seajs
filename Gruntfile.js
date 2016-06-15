b3fee9e7ffdf2a51bc474e6d1835b0f06125d1fc
YQ09 <zhuhjtcx@qq.com>
2016-06-15 13:38:07 +0800

    uglify

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,13 +5,38 @@ module.exports = function (grunt) {
             seajs: {
                 //输出文件的地址//后者是要合并的文件
                 files: {
-                    './app/dist/main.js': ['./app/public/js/drag.js', './app/public/js/limit.js', './app/public/js/util.js', './app/public/js/main.js']
+                    './app/dist/js/main.js': ['./app/.build/js/drag.js',
+                        './app/.build/js/limit.js',
+                        './app/.build/js/util.js',
+                        './app/.build/js/main.js']
+                }
+            }
+        },
+        //任务名字模块id的提取
+        transport:{
+            options:{//id头的配置./dist/js/drag.js
+                idleading:'./dist/'
+            },
+            seajs: {
+                files: {//
+                    './app/.build': ['./js/drag.js', './js/limit.js', './js/util.js', './js/main.js']
+                }
+            }
+        },
+        //压缩的处理
+        uglify:{
+            seajs: {
+                files: {//
+                    './app/dist/js/main.js': './app/dist/js/main.js'
                 }
             }
         }
     });
     //注册插件
     grunt.loadNpmTasks("grunt-contrib-concat");
+    grunt.loadNpmTasks("grunt-cmd-transport");
+    grunt.loadNpmTasks("grunt-contrib-uglify");
     //注册任务
-    grunt.registerTask("default",['concat'])
+    //grunt.registerTask("default",['concat']);
+    grunt.registerTask("default",['transport','concat','uglify']);
 };
